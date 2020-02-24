fb0c3ab53fbb276948829b151c9ea3eb213492a1
Charles Jacobs <cjacobs@microsoft.com>
2020-02-24 08:36:35 -0800

    Fixed various issues

--- index.js
+++ index.js
@@ -5,15 +5,12 @@ var Accessory, Service, Characteristic, UUIDGen
 module.exports = function (homebridge) {
     console.log("homebridge API version: " + homebridge.version);
 
-    // Accessory must be created from PlatformAccessory Constructor
-    // Accessory = homebridge.platformAccessory;
-
     // Service and Characteristic are from hap-nodejs
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
     UUIDGen = homebridge.hap.uuid;
 
-    homebridge.registerAccessory("homebridge-si7021", "Humidity and Temperature Sensor", Si7021Platform);
+    homebridge.registerAccessory("homebridge-si7021", "si7021Sensor", Si7021Platform);
 }
 
 // Accessory c'tor
@@ -45,6 +42,8 @@ class Si7021Platform {
             this.currentHumidity = data.humidity
             this.currentTemperatureC = data.temperature_C
         }).catch((err) => {
+            this.currentHumidity = Math.round(Math.random() * 100);
+            this.currentTemperatureC = Math.round(Math.random() * 10);
             this.log(`Error reading data: ${err}`)
         });
     }
@@ -53,23 +52,30 @@ class Si7021Platform {
         const informationService = new Service.AccessoryInformation();
     
         informationService
-          .setCharacteristic(Characteristic.Manufacturer, '???')
+          .setCharacteristic(Characteristic.Manufacturer, 'floydsoft')
           .setCharacteristic(Characteristic.Model, 'si7021 Temperature and Humidity Sensor')
-          .setCharacteristic(Characteristic.SerialNumber, 'Raspberry Pi');
+          .setCharacteristic(Characteristic.SerialNumber, 'none');
     
-        // Set up the service
-        this.service = new Service.HumiditySensor(this.name);
-        this.service
-          .getCharacteristic(Characteristic.CurrentHumidity)
+        // Set up the services
+        this.humidityService = new Service.HumiditySensor(this.name);
+        this.humidityService
+          .getCharacteristic(Characteristic.CurrentRelativeHumidity)
           .on('get', (callback) => {
             callback(null, this.currentHumidity);
           });
-        this.service
-          .getCharacteristic(Characteristic.CurrentTemperatureC)
+        this.humidityService
+          .getCharacteristic(Characteristic.Name)
+          .on('get', callback => {
+            callback(null, this.name);
+          });
+
+        this.temperatureService = new Service.TemperatureSensor(this.name);
+        this.temperatureService
+          .getCharacteristic(Characteristic.CurrentTemperature)
           .on('get', (callback) => {
             callback(null, this.currentTemperatureC);
           });
-        this.service
+        this.temperatureService
           .getCharacteristic(Characteristic.Name)
           .on('get', callback => {
             callback(null, this.name);
@@ -78,6 +84,6 @@ class Si7021Platform {
         // Begin reading info
         this.startReadLoop();
     
-        return [informationService, this.service];
+        return [informationService, this.humidityService, this.temperatureService];
       }
 }
